4b3ba9ca1b2c0bc6bedc2429b31e2718b1a8cfdb
LeezaJee <lisa.nadeem@outlook.de>
2022-03-28 00:11:21 +0200

    replaced sample with PokemonList variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
-alert('Hello world');
-let favoriteFood = 'Lasagna'; 
-document.write(favoriteFood);
+let PokemonList = [{name: "Charmander", height: 0.6, type: "fire"}, 
+{name: "Venusaur", height: 2, type: ["grass", "poison"]},
+{name: "Nidoking", height: 1.4, type: ["ground", "poison"]}];
