6ff7c6f4406717f0593dc8cd000ebcfdbb63a4c6
LeezaJee <lisa.nadeem@outlook.de>
2022-04-09 17:13:32 +0200

    adjusted code for repository

--- js/scripts.js
+++ js/scripts.js
@@ -15,14 +15,32 @@ const colors = {
 	normal: '#F5F5F5'
 }
 
-let pokemonRepository = (function() {
+var pokemonRepository = (function () {
 
-let pokemonList = [
-	{ name: "Charmander", height: 0.6, type: ["fire"] },
-	{ name: "Venusaur", height: 2, type: ["grass", "poison"] },
-	{ name: "Wartortle", height: 1, type: ["water"] },
-	{ name: "Nidoking", height: 1.4, type: ["ground", "poison"] }
-];
+	let repository = [
+		{
+			name: "Charmander",
+			height: 0.6,
+			type: ["fire"]
+		},
+		{
+			name: "Venusaur",
+			height: 2,
+			type: ["grass", "poison"]
+		},
+		{
+			name: "Wartortle",
+			height: 1,
+			type: ["water"]
+		},
+		{
+			name: "Nidoking",
+			height: 1.4,
+			type: ["ground", "poison"]
+		},
+	];
+
+
 
 function getAll() {
 	return pokemonList;
